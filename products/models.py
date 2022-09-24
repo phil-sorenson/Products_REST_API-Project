e32b0a5d26dc5f8739879f2ef96cc268e5faf191
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-24 02:04:15 -0500

    Fixed syntax errors in settings.py and models.py + migrated models.py data to MySQL

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Product(models.model):
+class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)

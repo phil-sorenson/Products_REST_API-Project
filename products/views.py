86ab5e58945e3d38e4cc46daec2c0afc6c86fbc7
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-25 00:08:32 -0500

    Created products_detail function in views.py + Gave products_detials function 'GET' by Id/pk functionality + Added additional URL path in prodcuts urls.py file (<int:pk>, views.products_list) + imported 'get_object_or_404 shortcut to views.py file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
@@ -16,4 +17,13 @@ def products_list(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def products_detail(request, pk ):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
     

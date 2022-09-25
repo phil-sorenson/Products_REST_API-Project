7ddbf1c255376d273fde51b6978fb537252e966c
phil-sorenson <phillip.sorenson31@gmail.com>
2022-09-24 23:01:19 -0500

    created a products object and serializer object in products_list function + created Superuser: phils + Updated admin.py with Product class + created Postman Get_All_Products collection

--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def products_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
 
-
-    return Response('ok')
+    
+    return Response(serializer.data)
